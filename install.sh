5943076597f9a6b15e90027fd32db58e30120072
Tiem Song <tiem@songfamily.org>
2012-10-31 14:08:53 -0700

    source bash_profile and bashrc, but only if files exist

--- install.sh
+++ install.sh
@@ -4,4 +4,10 @@ ln -fs `pwd -P`/dotfiles/bash_aliases ~/.bash_aliases
 ln -fs `pwd -P`/dotfiles/gitconfig ~/.gitconfig
 ln -fs `pwd -P`/dotfiles/tmux.conf ~/.tmux.conf
 
-source ~/.bash_profile
+if [ -r ~/.bash_profile ]; then
+  source ~/.bash_profile
+fi
+
+if [ -r ~/.bashrc ]; then
+  source ~/.bashrc
+fi
