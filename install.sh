1931864fd5af749e650c1f9c66201dcbcd25de2f
Tiem Song <tiem@songfamily.org>
2014-02-21 13:43:44 -0800

    move gui settings to new gvimrc file.  update install script to link gvimrc file.  remove vim directory (not currently using)

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 ln -fs `pwd -P`/dotfiles/bash_aliases ~/.bash_aliases
 ln -fs `pwd -P`/dotfiles/gitconfig ~/.gitconfig
+ln -fs `pwd -P`/dotfiles/gvimrc ~/.gvimrc
 ln -fs `pwd -P`/dotfiles/tmux.conf ~/.tmux.conf
-ln -fs `pwd -P`/dotfiles/vim ~/.vim
 ln -fs `pwd -P`/dotfiles/vimrc ~/.vimrc
 
 if [ -r ~/.bash_profile ]; then
