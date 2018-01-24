aa5aff44413112b023c4533e323a4f96a257b271
Tiem Song <tiem@songfamily.org>
2018-01-24 14:45:59 -0800

    Update gitconfig and re-add sourcing gitconfig in install script

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 ln -fs `pwd -P`/dotfiles/bash_common ~/.bash_common
 
 # configs for various programs
+ ln -fs `pwd -P`/dotfiles/gitconfig ~/.gitconfig
 ln -fs `pwd -P`/dotfiles/tmux.conf ~/.tmux.conf
 
 # graveyard
-# ln -fs `pwd -P`/dotfiles/gitconfig ~/.gitconfig
 # ln -fs `pwd -P`/dotfiles/vimrc.after ~/.vimrc.after
