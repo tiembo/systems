5392d7a43847e38323b4d0091e3f38aab4ac792d
Tiem Song <tiem@google.com>
2019-02-28 10:22:30 -0800

    add git bash completion

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 ln -fs `pwd -P`/dotfiles/bash_common ~/.bash_common
 
 # configs for various programs
- ln -fs `pwd -P`/dotfiles/gitconfig ~/.gitconfig
+ln -fs `pwd -P`/dotfiles/git-completion.bash ~/.git-completion.bash
+ln -fs `pwd -P`/dotfiles/gitconfig ~/.gitconfig
 ln -fs `pwd -P`/dotfiles/tmux.conf ~/.tmux.conf
 ln -fs `pwd -P`/dotfiles/vimrc.after ~/.vimrc.after
